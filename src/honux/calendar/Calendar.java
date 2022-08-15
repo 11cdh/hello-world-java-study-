aa92591d6501f326751ab4a4b11be349fa90f88b
11cdh <104803789+11cdh@users.noreply.github.com>
2022-08-15 21:40:18 +0900

    aa

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,8 +1,14 @@
 package honux.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
+
+		int month;
+		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		String s1;
 		System.out.println(" 일 월  화 수  목 금 토");
 		System.out.println("-------------------- ");
 		System.out.println(" 1  2  3  4  5  6  7 ");
@@ -10,6 +16,14 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21 ");
 		System.out.println("22 23 24 25 26 27 28 ");
 
-		
+		System.out.println("달을 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+
+		s1 = scanner.next();
+		month = Integer.parseInt(s1);
+
+		System.out.printf("%d월은 %d일 까지 있습니다. \n", month, maxDays[month - 1]);
+		scanner.close();
+
 	}
 }
